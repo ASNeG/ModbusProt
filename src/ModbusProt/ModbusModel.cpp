cdef2b2cb7c5b4402c7b4c410f55e5238de17999
Kai Huebl <kai@huebl-sgh.de>
2025-01-15 03:43:51 +0100

    add modbus model

--- src/ModbusProt/ModbusModel.cpp
+++ src/ModbusProt/ModbusModel.cpp
@@ -20,12 +20,256 @@
 namespace ModbusProt
 {
 
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	//
+	// MemoryArea
+	//
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	MemoryArea::MemoryArea(
+		MemoryType memoryType,
+		uint16_t startAddress,
+		uint16_t numValues
+	)
+	{
+		memoryType_ = memoryType;
+		startAddress_ = startAddress;
+		numValues_ = numValues;
+
+		if (memoryType_ == MemoryType::Coils || memoryType == MemoryType::Inputs) valueSizeBits_ = 1;
+		valueSizeBits_ = 16;
+
+		memLen_ = ((numValues_ * valueSizeBits_) + 7) / 8;
+		memData_ = (char*)malloc(memLen_);
+	}
+
+	MemoryArea::~MemoryArea(void)
+	{
+		if (memData_ != nullptr) {
+			delete memData_;
+		}
+
+		memData_ = nullptr;
+		memLen_ = 0;
+	}
+
+	bool
+	MemoryArea::setValue(uint16_t address, uint8_t* value, uint16_t numValues)
+	{
+		if (address < startAddress_  || address + numValues >= startAddress_ + numValues_) return false;
+
+		if (valueSizeBits_ == 1) {
+			bool* val = (bool*)value;
+			for (uint16_t idx = 0; idx < numValues; idx++) {
+				setValue(address+idx, val[idx]);
+			}
+		}
+		else {
+			uint16_t* val = (uint16_t*)value;
+			for (uint16_t idx = 0; idx < numValues; idx++) {
+				setValue(address+idx, val[idx]);
+			}
+		}
+
+		return true;
+	}
+
+	bool
+	MemoryArea::getValue(uint16_t address, uint8_t* value, uint16_t numValues)
+	{
+		if (address < startAddress_  || address + numValues >= startAddress_ + numValues_) return false;
+
+		if (valueSizeBits_ == 1) {
+			bool* val = (bool*)value;
+			for (uint16_t idx = 0; idx < numValues; idx++) {
+				getValue(address+idx, val[idx]);
+			}
+		}
+		else {
+			uint16_t* val = (uint16_t*)value;
+			for (uint16_t idx = 0; idx < numValues; idx++) {
+				getValue(address+idx, val[idx]);
+			}
+		}
+
+		return true;
+	}
+
+	bool
+	MemoryArea::setValue(uint16_t address, bool value)
+	{
+		uint16_t offset = (address - startAddress_) / 8;
+		uint8_t rest = (address - startAddress_) % 8;
+
+		if (value) {
+			memData_[offset] = memData_[offset] | (1 << rest);
+		}
+		else {
+			memData_[offset] = memData_[offset] & ~(1 << rest);
+		}
+
+		return true;
+	}
+
+	bool
+	MemoryArea::setValue(uint16_t address, uint16_t value)
+	{
+		uint16_t offset = (address - startAddress_);
+		((uint16_t*)memData_)[offset] = value;
+		return true;
+	}
+
+	bool
+	MemoryArea::getValue(uint16_t address, bool& value)
+	{
+		uint16_t offset = (address - startAddress_) / 8;
+		uint8_t rest = (address - startAddress_) % 8;
+
+		if ((memData_[offset] & (1 << rest)) != 0) {
+			value = 1;
+		}
+		else {
+			value = 0;
+		}
+
+		return true;
+	}
+
+	bool
+	MemoryArea::getValue(uint16_t address, uint16_t& value)
+	{
+		uint16_t offset = (address - startAddress_);
+		value = ((uint16_t*)memData_)[offset];
+		return true;
+	}
+
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	//
+	// ModbusModel
+	//
+	// ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
 	ModbusModel::ModbusModel(void)
 	{
 	}
 
 	ModbusModel::~ModbusModel(void)
 	{
+	}
+
+	bool
+	ModbusModel::registerMemoryArea(
+		MemoryType memoryType,
+		uint16_t startAddress,
+		uint16_t numValues
+	)
+	{
+		// Create memory area
+		auto memoryArea = std::make_shared<MemoryArea>(memoryType, startAddress, numValues);
+
+		// Register memory area
+		switch (memoryType) {
+			case MemoryType::Coils:
+			{
+				memoryAreaCoils_ = memoryArea;
+				break;
+			}
+			case MemoryType::Inputs:
+			{
+				memoryAreaInputs_ = memoryArea;
+				break;
+			}
+			case MemoryType::InputRegisters:
+			{
+				memoryAreaInputRegisters_ = memoryArea;
+				break;
+			}
+			case MemoryType::HoldingRegisters:
+			{
+				memoryAreaHoldingRegisters_ = memoryArea;
+				break;
+			}
+			default:
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	bool
+	ModbusModel::setValue(MemoryType memoryType, uint16_t startAddress, uint8_t* values, uint16_t numValues)
+	{
+		switch (memoryType) {
+			case MemoryType::Coils:
+			{
+				if (memoryAreaCoils_ == nullptr) return false;
+				return memoryAreaCoils_->setValue(startAddress, values, numValues);
+				break;
+			}
+			case MemoryType::Inputs:
+			{
+				if (memoryAreaInputs_ == nullptr) return false;
+				return memoryAreaInputs_->setValue(startAddress, values, numValues);
+				break;
+			}
+			case MemoryType::InputRegisters:
+			{
+				if (memoryAreaInputRegisters_ == nullptr) return false;
+				return memoryAreaInputRegisters_->setValue(startAddress, values, numValues);
+				break;
+			}
+			case MemoryType::HoldingRegisters:
+			{
+				if (memoryAreaHoldingRegisters_ == nullptr) return false;
+				return memoryAreaHoldingRegisters_->setValue(startAddress, values, numValues);
+				break;
+			}
+			default:
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool
+	ModbusModel::getValue(MemoryType memoryType, uint16_t startAddress, uint8_t* values, uint16_t numValues)
+	{
+		switch (memoryType) {
+			case MemoryType::Coils:
+			{
+				if (memoryAreaCoils_ == nullptr) return false;
+				return memoryAreaCoils_->getValue(startAddress, values, numValues);
+				break;
+			}
+			case MemoryType::Inputs:
+			{
+				if (memoryAreaInputs_ == nullptr) return false;
+				return memoryAreaInputs_->getValue(startAddress, values, numValues);
+				break;
+			}
+			case MemoryType::InputRegisters:
+			{
+				if (memoryAreaInputRegisters_ == nullptr) return false;
+				return memoryAreaInputRegisters_->getValue(startAddress, values, numValues);
+				break;
+			}
+			case MemoryType::HoldingRegisters:
+			{
+				if (memoryAreaHoldingRegisters_ == nullptr) return false;
+				return memoryAreaHoldingRegisters_->getValue(startAddress, values, numValues);
+				break;
+			}
+			default:
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
--- src/ModbusProt/ModbusModel.h
+++ src/ModbusProt/ModbusModel.h
@@ -18,23 +18,67 @@
 #ifndef __ModbusProt_ModbusModel_h__
 #define __ModbusProt_ModbusModel_h__
 
+#include <memory>
+
 namespace ModbusProt
 {
 
-	enum class ModbusMemoryType {
+	enum class MemoryType {
 		Coils,
 		Inputs,
 		InputRegisters,
 		HoldingRegisters
 	};
 
+	class MemoryArea
+	{
+	  public:
+		using SPtr = std::shared_ptr<MemoryArea>;
+
+		MemoryArea(
+			MemoryType memoryType,
+			uint16_t startAddress,
+			uint16_t numValues
+		);
+		~MemoryArea(void);
+
+		bool setValue(uint16_t address, uint8_t* value, uint16_t numValues);
+		bool getValue(uint16_t address, uint8_t* value, uint16_t numValues);
+
+	  private:
+		MemoryType memoryType_ = MemoryType::Coils;
+		uint16_t startAddress_ = 0;
+		uint32_t numValues_ = 0;
+		uint8_t valueSizeBits_ = 0;
+
+		char* memData_ = nullptr;
+		uint16_t memLen_ = 0;
+
+		bool setValue(uint16_t address, bool value);
+		bool setValue(uint16_t address, uint16_t value);
+		bool getValue(uint16_t address, bool& value);
+		bool getValue(uint16_t address, uint16_t& value);
+	};
+
 	class ModbusModel {
 	  public:
-		void
 		ModbusModel(void);
 		virtual ~ModbusModel(void);
 
+		bool registerMemoryArea(
+			MemoryType memoryType,
+			uint16_t startAddress,
+			uint16_t numValues
+		);
+		bool setValue(MemoryType memoryType, uint16_t startAddress, uint8_t* values, uint16_t numValues);
+		bool getValue(MemoryType memoryType, uint16_t startAddress, uint8_t* values, uint16_t numValues);
+
 	  private:
+		MemoryArea::SPtr memoryAreaCoils_ = nullptr;
+		MemoryArea::SPtr memoryAreaInputs_ = nullptr;
+		MemoryArea::SPtr memoryAreaInputRegisters_ = nullptr;
+		MemoryArea::SPtr memoryAreaHoldingRegisters_ = nullptr;
+	};
 }
 
 #endif
